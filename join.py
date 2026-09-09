21629578e3a1b944deebc7b45eba474a3433cdf1
orangishcat <fdsnail2@gmail.com>
2026-09-09 16:30:30 -0700

    fix: update join.py and rename intro for natsort

--- join.py
+++ join.py
@@ -1,5 +1,13 @@
+# /// script
+# dependencies = [
+#   "natsort"
+# ]
+# ///
+
 """
-combines all the md files in a directory into one big one that i can post on social media
+combines all the md files in a directory into one big one that i can post on social media.
+
+run using `uv run join.py` because uv can pull in the `natsort` package temporarily for use in this script
 """
 
 import re
@@ -7,11 +15,11 @@ from pathlib import Path
 
 from natsort import natsorted
 
-START_FOLDER = "personal-website"
+START_FOLDER = input("Enter start folder: ")
 PROJECT_DIR = Path(__file__).parent
 START_DIR = PROJECT_DIR / "src" / "content" / "blog" / START_FOLDER
 TITLE_RE = re.compile("---\ntitle: (.+)\n---", re.MULTILINE | re.DOTALL)
-IMAGE_RE = re.compile(r"!\[\[.\/attachments\/(.+)\]\]")
+IMAGE_RE = re.compile(r"!\[\[.\/attachments\/(.+)\]\]", re.IGNORECASE)
 WIDTH_RE = re.compile(r"\|\d+")
 
 
@@ -33,5 +41,6 @@ def read_file(filename: Path):
 
 
 files = natsorted(START_DIR.glob("*.md"))
-with open(PROJECT_DIR / "out.md", "w") as w:
+with open(path := PROJECT_DIR / "out.md", "w") as w:
     w.write("\n\n---\n\n".join(read_file(f) for f in files))
+print(f"Joined markdown file written to {path}")
